61725d44a0669fd093130bfdec7c555690d966e0
aleksei24 <lexa24032@gmail.com>
2020-02-21 12:44:16 +0300

    new component has been made

---
+++ test-vue-cli/src/components/Test.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="test">
+        <h1>{{message}}</h1>
+        <p>
+            Some text should be put here,
+            but later
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    props: {
+        message: String
+    }
+}
+</script>
+
+<style scoped>
+    .test{
+        max-width: 300px;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    h1{
+        color: skyblue;
+        font-size: 24px;
+        letter-spacing: 5px;
+    }
+    p{
+        color: chocolate;
+        font-size: 18px;
+    }
+</style>
