9e86937d688f3f89653730ef61368f18eddc3c46
aleksei24 <lexa24032@gmail.com>
2020-02-21 13:02:33 +0300

    new branch with a new app

--- test-vue-cli/src/App.vue
+++ test-vue-cli/src/App.vue
@@ -1,31 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Test message="I`m a new one!!!"/>
+  <div id="new App">
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Test from './components/Test.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    Test
+  name: ' new App',
+
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
